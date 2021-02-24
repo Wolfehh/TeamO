ff212cc8f91f637939c3e51d7e10553cc76e2180
Maxwell <mjwalker@mtu.edu>
2021-02-24 11:26:23 -0500

    Added method stubs + edited manifest to allow permissions for notification listener

---
+++ MyApplication/app/src/main/java/com/teamo/myapplication/NListener.java
@@ -0,0 +1,46 @@
+package com.teamo.myapplication;
+
+
+import android.os.Build;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class NListener extends NotificationListenerService
+{
+
+    private String TAG = this.getClass().getSimpleName();
+
+    @Override
+    public void onCreate()
+    {
+
+    }
+
+    @Override
+    public void onDestroy()
+    {
+
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sNotif)
+    {
+        Log.i(TAG, ":Notification Removed");
+        Log.i(TAG, "ID: " + sNotif.getId() + "\t" + sNotif.getNotification().tickerText + "\t" + sNotif.getPackageName());
+        
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sNotif)
+    {
+
+    }
+
+
+
+}
