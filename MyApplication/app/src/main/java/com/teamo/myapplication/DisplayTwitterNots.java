5ab6b029b64dd252db280ad984b7ccce17f980c3
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2021-02-24 18:24:15 -0500

    Twitter Notification Maker is WORKING
    - Creates a textview everytime its called

--- MyApplication/app/src/main/java/com/teamo/myapplication/DisplayTwitterNots.java
+++ MyApplication/app/src/main/java/com/teamo/myapplication/DisplayTwitterNots.java
@@ -2,13 +2,38 @@ package com.teamo.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayTwitterNots extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_twitter_nots);
+        LinearLayout linearLayout = new LinearLayout(this); // Creates a linear layout when the apps created?
+        createNotification(linearLayout,"Spaghetti");
+        createNotification(linearLayout,"Spaghetti.");
+        createNotification(linearLayout,"Spaghetti!");
+        createNotification(linearLayout,"Spaghetti?");
+        createNotification(linearLayout,"Spaghetti!!!");
+        createNotification(linearLayout,"Spaghetti :(");
+        createNotification(linearLayout,"Spaghetti :)");
+
+    }
+
+    void createNotification(LinearLayout linearLayout, String notification){
+        setContentView(linearLayout);   // Sets to the linear layout we create in onCreate
+        linearLayout.setOrientation(LinearLayout.VERTICAL); // Ensures the layout is a vertical one and not a horizontal one
+        String theNotification = "Notification: " + notification;   // Concatenates the text so it says Notification:
+        TextView textView = new TextView(this); // Creates a new textbox in the activity context
+        int id = (int)System.currentTimeMillis();   // Creates an id based off the time that it was created
+        textView.setId(id); // Sets an id. Don't know if we will need it, but its something incase
+        textView.setWidth(linearLayout.getWidth()); // Sets width
+        textView.setHeight(79); // Sets height
+        textView.setText(theNotification);  // Sets text of the textview
+        linearLayout.addView(textView); // Adds textview to the screen
     }
 }
