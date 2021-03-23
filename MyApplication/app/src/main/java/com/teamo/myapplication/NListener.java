3c2b2b2e97af4a2c01d920266e90d795f012d8b4
Ethan Fournier <59780951+Wolfehh@users.noreply.github.com>
2021-03-23 18:30:17 -0400

    Adds notifications from main activity

--- MyApplication/app/src/main/java/com/teamo/myapplication/NListener.java
+++ MyApplication/app/src/main/java/com/teamo/myapplication/NListener.java
@@ -60,6 +60,7 @@ public class NListener extends NotificationListenerService
         //example broadcast - onNotificationPosted:com.snapchat.android
         Intent post = new Intent("com.teamo.myapplication.NOTIFICATION_LISTENER");
         post.putExtra("notification_event", "onNotificationPosted:" + sNotif.getPackageName() + "\n");
+        post.putExtra("notification", "" + sNotif.getNotification().tickerText);
         sendBroadcast(post);
 
 
